63b528d49809f418864e8fdc2bfc9acaa48413f5
MihailDmitrenko <36im@mail.ru>
2017-02-04 00:51:48 +0400

    Singolo

---
+++ js/images-sort.js
@@ -0,0 +1,32 @@
+
+
+/* When the DOM-tree is ready the tag ALL is Active */
+
+$(document).ready(function(){
+	$('.WB, .GD, .Ar').addClass('')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+	$('#WB').click(function(){
+		$('.GD, .Ar').addClass('imege-sort')
+	});
+});
+
